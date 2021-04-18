85dac2b35955b2b8cb26a655c99c11dcaeaff86a
Kostikans <48389219+Kostikans@users.noreply.github.com>
2021-04-18 18:56:07 +0300

    init

---
+++ app/src/main/java/main/src/etherscan/MainActivity.kt
@@ -0,0 +1,11 @@
+package main.src.etherscan
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
