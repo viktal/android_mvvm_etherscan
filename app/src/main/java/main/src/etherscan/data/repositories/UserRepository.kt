820876321afaed4b00a03a1230ed3af6890b9eaa
Kostikans <48389219+Kostikans@users.noreply.github.com>
2021-04-25 17:12:21 +0300

    working activity

--- app/src/main/java/main/src/etherscan/data/repositories/UserRepository.kt
+++ app/src/main/java/main/src/etherscan/data/repositories/UserRepository.kt
@@ -6,11 +6,6 @@ class UserRepository() {
     val network = EthplorerRemoteStorage()
     private val apiParams = "?apiKey=EK-3HTzj-zcxPo7d-qCssY&showETHTotals=true"
 
-    suspend fun loginByMnemonic(mnemonic: String): AddressInfoModel? {
-        return network.getAddressInfo(apiParams)
-    }
-    suspend fun  loginByAddress(address: String): AddressInfoModel? {
-        return network.getAddressInfo(address+apiParams)
-    }
+
 
 }
